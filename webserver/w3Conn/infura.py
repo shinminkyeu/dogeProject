d7a3229b4919686aaf77f0918f80fe8f740b1d37
Jaye <carsurta@gmail.com>
2019-05-22 14:28:47 +0900

    call까지는 했는데

---
+++ webserver/w3Conn/infura.py
@@ -0,0 +1,3 @@
+from web3 import Web3
+
+web3 = Web3(Web3.WebsocketProvider("wss://ropsten.infura.io/we/v3/6de59f77f06e447a89d8fecdcbc9017d"))
--- webserver/w3Conn/views.py
+++ webserver/w3Conn/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 
+from .infura import web3
+from dog.models import *
+from user.models import *
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        return redirect(request)
+    else:
+        context = {
+
+        }
     return render(request, 'w3Conn/w3Test.html')
