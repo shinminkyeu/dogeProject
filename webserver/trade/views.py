efbcf41d297e40ae8210ade5f2d50376abf03c01
Jaye <carsurta@gmail.com>
2019-05-26 20:48:15 +0900

    w3conn 삭제 준비

--- webserver/trade/views.py
+++ webserver/trade/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+
+from contract import checkSign
+from .models import *
 
 # Create your views here.
