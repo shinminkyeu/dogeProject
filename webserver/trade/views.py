32f570e8d883bc9dd0c7434a06f6693f4e31449a
Jaye <carsurta@gmail.com>
2019-05-27 16:54:57 +0900

    레이아웃 업데이트

--- webserver/trade/views.py
+++ webserver/trade/views.py
@@ -8,10 +8,16 @@ from dog.models import *
 
 # Create your views here.
 class searchForm(forms.Form):
-    breed = forms.TypedChoiceField()
+    gender_choice = ((False, '수컷'), (True, '암컷'))
+    breed = forms.TypedChoiceField(coerce = int)
+    ageStart = forms.IntegerField(min_value = 0)
+    ageEnd = forms.IntegerField(min_value = 0)
+    gender = forms.TypedChoiceField(choices = gender_choice, coerce = bool)
 
 def index(request):
-    search = 'b'
+    search = {
+        'breed': Breed
+    }
     content = 'c'
     context = {
         # 사이드 컨텍스트 메뉴에 들어갈 콘텐츠
@@ -19,3 +25,4 @@ def index(request):
         # 본문에 들어갈 콘텐츠
         'content': content
     }
+    return render(request, 'index.html', context = context)
