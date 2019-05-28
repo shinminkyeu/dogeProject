2b3b4040c8c5066650eca133601728f31ccb14d2
Jaye <carsurta@gmail.com>
2019-05-28 11:33:20 +0900

    테스트 코드 작성

--- webserver/trade/views.py
+++ webserver/trade/views.py
@@ -16,13 +16,13 @@ class searchForm(forms.Form):
     gender = forms.TypedChoiceField(choices = gender_choice, coerce = bool)
 
 def index(request):
-    waiting_indexs = [0] # reversed(contract.functions.showWattingTrade()[-10:])
+    waiting_indexs = [0] # reversed(contract.functions.showWattingTrade().call()[-10:])
     waiting_list = []
-    for i in range(6):
-        print(contract.functions.showTrade(0)[i])
     for id in waiting_indexs:
-        waiting_list.append(contract.functions.showTrade(id))
+        waiting_list.append(contract.functions.showTrade(id).call())
         print(waiting_list[0])
+        for i in range(6):
+            print(waiting_list[0][i])
     context = {
         # 사이드 컨텍스트 메뉴에 들어갈 콘텐츠
         'breeds': getBreed(),
