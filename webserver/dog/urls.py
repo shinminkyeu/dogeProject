7238062c6cd3869d9965afafe959b098326b6266
shinminkyeu <smk940721@gmail.com>
2019-05-29 16:24:34 +0900

    MK_commit

--- webserver/dog/urls.py
+++ webserver/dog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.findMyDog),
     path('register/', views.register),
+    path('dogInfo/', views.info),
 ]
